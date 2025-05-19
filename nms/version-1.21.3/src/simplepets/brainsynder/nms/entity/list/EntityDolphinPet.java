72caa90809fb920998b8825e8169cee76df494bb
brainsynder <briansnyder498@gmail.com>
2025-05-18 22:18:19 -0400

    fix: dolphin pet not working on 1.21.5

--- nms/version-1.21.3/src/simplepets/brainsynder/nms/entity/list/EntityDolphinPet.java
+++ nms/version-1.21.3/src/simplepets/brainsynder/nms/entity/list/EntityDolphinPet.java
@@ -1,5 +1,6 @@
 package simplepets.brainsynder.nms.entity.list;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -14,6 +15,7 @@ import simplepets.brainsynder.nms.utils.PetDataAccess;
  * NMS: {@link net.minecraft.world.entity.animal.Dolphin}
  */
 public class EntityDolphinPet extends EntityAgeablePet implements IEntityDolphinPet {
+    private static final EntityDataAccessor<BlockPos> TREASURE = SynchedEntityData.defineId(EntityDolphinPet.class, EntityDataSerializers.BLOCK_POS);
     private static final EntityDataAccessor<Boolean> HAS_FISH = SynchedEntityData.defineId(EntityDolphinPet.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Integer> MOIST = SynchedEntityData.defineId(EntityDolphinPet.class, EntityDataSerializers.INT);
 
@@ -24,6 +26,7 @@ public class EntityDolphinPet extends EntityAgeablePet implements IEntityDolphin
     @Override
     public void populateDataAccess(PetDataAccess dataAccess) {
         super.populateDataAccess(dataAccess);
+        dataAccess.define(TREASURE, BlockPos.ZERO);
         dataAccess.define(HAS_FISH, false);
         dataAccess.define(MOIST, 2400);
     }
