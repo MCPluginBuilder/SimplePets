f54fdfa4ce227fa42e2345a3ac3497e37b1442fa
brainsynder <briansnyder498@gmail.com>
2025-11-19 19:36:15 -0500

    fix: Camel Husk not having the correct interface

--- nms/master/src/simplepets/brainsynder/nms/entity/list/EntityCamelHuskPet.java
+++ nms/master/src/simplepets/brainsynder/nms/entity/list/EntityCamelHuskPet.java
@@ -4,7 +4,7 @@ import lib.brainsynder.ServerVersion;
 import lib.brainsynder.SupportedVersion;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
-import simplepets.brainsynder.api.entity.passive.IEntityCamelPet;
+import simplepets.brainsynder.api.entity.passive.IEntityCamelHuskPet;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.user.PetUser;
 
@@ -12,7 +12,7 @@ import simplepets.brainsynder.api.user.PetUser;
  * NMS: {@link net.minecraft.world.entity.animal.camel.CamelHusk}
  */
 @SupportedVersion(version = ServerVersion.v1_21_11)
-public class EntityCamelHuskPet extends EntityCamelPet implements IEntityCamelPet {
+public class EntityCamelHuskPet extends EntityCamelPet implements IEntityCamelHuskPet {
     public EntityCamelHuskPet(PetType type, PetUser user) {
         this(EntityType.CAMEL, type, user); // TODO: Update EntityType
     }
